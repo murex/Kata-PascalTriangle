74eed0e99ea829ed8742ce20cdbdfe7f8ea684ee
Damien Menanteau <damien.menanteau@gmail.com>
2023-06-21 12:12:37 +0200

    Use text block in java's acceptance test expected result

--- java/src/test/java/com/murex/PascalTriangleTest.java
+++ java/src/test/java/com/murex/PascalTriangleTest.java
@@ -33,15 +33,16 @@ public class PascalTriangleTest {
     @Test
     public void acceptance_test() {
         int lastLine = 7;
-        String expected = ""
-                + "1\n"
-                + "1 1\n"
-                + "1 2 1\n"
-                + "1 3 3 1\n"
-                + "1 4 6 4 1\n"
-                + "1 5 10 10 5 1\n"
-                + "1 6 15 20 15 6 1\n"
-                + "1 7 21 35 35 21 7 1\n";
+        String expected = """
+                1
+                1 1
+                1 2 1
+                1 3 3 1
+                1 4 6 4 1
+                1 5 10 10 5 1
+                1 6 15 20 15 6 1
+                1 7 21 35 35 21 7 1
+                """;
         assertEquals(expected, PascalTriangle.draw(lastLine));
     }
 }
