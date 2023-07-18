bfc16fc412000d9f5e74da8acb0045fd87fa49cc
Ahmad ATWI <aatwi@murex.com>
2023-07-18 16:18:05 +0200

    Add python code

--- python/src/pascal_triangle/pascal_triangle.py
+++ python/src/pascal_triangle/pascal_triangle.py
@@ -17,7 +17,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import string
 
-# TODO Replace with something more relevant for the kata
-def do_something() -> int:
-    return 42
+
+def draw(last_line) -> string:
+    return ""
